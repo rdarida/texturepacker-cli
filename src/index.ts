40d774e96ad8ede534c60e7b07c26db9115ab0c1
Róbert Darida <rdarida@users.noreply.github.com>
2024-12-13 00:36:05 +0100

    chore: add comment to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 /**
- * @returns The greeting.
+ * Returns a greeting message for TexturePacker.
+ * @returns A string containing the greeting message.
  */
 export function greet(): string {
   return 'Hello, TexturePacker!';
